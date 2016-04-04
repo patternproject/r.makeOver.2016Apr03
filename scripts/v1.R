05b70c7232307ce1f3a6fe615694d36d0f0fe7ba
patternproject <patternproject@yahoo.com>
2016-04-04 09:15:40 +0000

    changes in the script file, mainly related to data pocessing

--- scripts/v1.R
+++ scripts/v1.R
@@ -2,9 +2,10 @@
 ## Load Libraries ####
 library(dplyr)
 library(ggplot2)
+library(tidyr)
 
 ## Read Data ####
-fp = "/mnt/2016 Apr 03/Data/Urban Diversity in America v6.csv"
+fp = "/mnt/r.makeOver.2016Apr03/data/Urban Diversity in America v6.csv"
 df.raw = read.csv(file=fp, stringsAsFactors = FALSE)
 
 ## Data Manipulations ####
@@ -18,4 +19,14 @@ df.raw %>%
             minI = min(IntegrationI), maxI = max(IntegrationI), rngI = maxI-minI,
             minRngAll = min(rngC,rngN,rngI)) 
 
+df.1 = df.raw
+# as Integration has the lowest range (0.3), use it to segment the other two 
+# divide this range into 5 chunks
+
+# intI has the chunk value, ranging from 1 to 5
+df.1$intervalI = cut(df.1$IntegrationI, breaks=5, include.lowest=TRUE, labels=c(1,2,3,4,5))
+
+df.2 <- df.1 %>% gather(metric, value, 3:6)
+
 ## Visulization ####
+# draw a bar plot for CityI and NeighborhoodI, faceting on IntegrationI
